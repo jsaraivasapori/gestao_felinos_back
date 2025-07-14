d367fb2a028cac7b62f57b957767227333b45564
joaosaraivasapori@gmail.com <joaosaraivasapori@gmail.com>
2025-07-13 22:23:16 -0300

    delet

--- src/vacinas/dto/vacinacao/update-protocolo-vacinal-status-edit.dto.ts
+++
@@ -1,8 +0,0 @@
-import { StatusCiclo } from '@prisma/client';
-import { IsEnum, IsNotEmpty } from 'class-validator';
-
-export class UpdateProtocoloVacinalStatus {
-  @IsEnum(StatusCiclo)
-  @IsNotEmpty()
-  status: StatusCiclo;
-}
