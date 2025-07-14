7785c9069ea41f82bedae82b7dea1c8f35d73bcc
joaosaraivasapori@gmail.com <joaosaraivasapori@gmail.com>
2025-07-13 22:20:34 -0300

    .

--- src/vacinas/dto/update-vacina.dto.ts
+++ src/vacinas/dto/update-vacina.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateVacinaDto } from './create-vacina.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateVacinaDto extends PartialType(CreateVacinaDto) {}
+export class UpdateVacinaDto extends PartialType(CreateVacinaDto) {
+  @IsOptional()
+  name: string;
+}
