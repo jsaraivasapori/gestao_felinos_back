e8841052267062a16b67cf07a39fdc61e25a5464
João <joaosaraivasapori@gmail.com>
2025-07-15 15:02:17 -0300

    adicionado controllers sobre a vacinacao

--- src/vacinas/vacinas.controller.ts
+++ src/vacinas/vacinas.controller.ts
@@ -10,11 +10,33 @@ import {
 import { VacinasService } from './vacinas.service';
 import { CreateVacinaDto } from './dto/create-vacina.dto';
 import { UpdateVacinaDto } from './dto/update-vacina.dto';
+import { CreateVacinacaoDto } from './dto/vacinacao/vacinacao-create-dto';
+import { RegistrarDoseSubsequenteDto } from './dto/vacinacao/vacinacao-subsequente-create-dto';
 
 @Controller('vacinas')
 export class VacinasController {
   constructor(private readonly vacinasService: VacinasService) {}
 
+  //====================================================================================
+  // CONTROLADORES DE VACINAÇÃO. CRIAR PRIEMIRA DOSE E CRIAR DOSES SUBSEQUENTES
+  //====================================================================================
+  @Post('vacinacao')
+  async createNewVaccination(@Body() createVacinacaoDto: CreateVacinacaoDto) {
+    return this.vacinasService.registrarPrimeiraDose(createVacinacaoDto);
+  }
+
+  @Post('vacinacao/nova-dose')
+  async createNewDose(
+    @Body() registrarDoseSubsequenteDto: RegistrarDoseSubsequenteDto,
+  ) {
+    return this.vacinasService.aplicarDosesSubsequentes(
+      registrarDoseSubsequenteDto,
+    );
+  }
+
+  //==========================================================================================
+  // Controladores para Vacinas. Criar, mostrar e editar cadastro de vacinas no sistema
+  //==========================================================================================
   @Post()
   async create(@Body() createVacinaDto: CreateVacinaDto) {
     return this.vacinasService.create(createVacinaDto);
